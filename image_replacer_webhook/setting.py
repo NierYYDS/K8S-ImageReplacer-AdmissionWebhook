2594059c41043d3552b81703d08f58c79b5bd9e8
NierYYDS <jackieqiuchou@gmail.com>
2024-04-24 16:16:06 +0800

    fix:  using typing List for  py3.8 compatible

--- image_replacer_webhook/setting.py
+++ image_replacer_webhook/setting.py
@@ -2,6 +2,7 @@
 Author: NierYYDS
 """
 
+from typing import List
 from pydantic_settings import BaseSettings
 
 
@@ -12,7 +13,7 @@ class Settings(BaseSettings):
     ignore_ca_cert: bool = False  # 服务启动时不依赖CA证书，用于k8s部署
     cache_enabled: bool = True
     cache_registry: str = "m.daocloud.io"
-    skip_registries: list[str] = []  # 不进行替换的源
+    skip_registries: List[str] = []  # 不进行替换的源
 
 
 settings = Settings()
